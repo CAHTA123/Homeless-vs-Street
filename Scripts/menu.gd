9adb4f954dfe7306d6854dbbd2a65c6892d9665c
CAHTA123 <135743906+CAHTA123@users.noreply.github.com>
2024-07-12 02:42:44 +0500

    1

---
+++ Scripts/menu.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var speed = 100
+
+func _process(delta):
+	$BG_Menu.scroll_offset.x -= speed * delta
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://Scenes/first.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
---
+++ Scripts/player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_walking = false
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		if not is_walking:
+			$Anim.play("WALK")
+			is_walking = true
+		$Anim.scale.x = direction 
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_walking:
+			$Anim.play("IDLE")
+			is_walking = false
+
+	move_and_slide()
+
+
+func _on_animated_sprite_2d_animation_finished():
+	is_walking = false
