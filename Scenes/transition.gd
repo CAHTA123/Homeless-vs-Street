2b9e4db3f988ce9b2f57aeca680fa7fb8947f849
IdeaBanana <pirate228kek@gmail.com>
2024-07-14 06:03:13 +0300

    InitialBanana

--- Scenes/transition.gd
+++ Scenes/transition.gd
@@ -5,12 +5,14 @@ extends Node2D
 var current_index: int = 0
 
 func _ready():
-	var current_scene_path = get_tree().current_scene.get_name()
+	var current_scene_path: = get_tree().current_scene.get_name()
 	if scenes.size() > 0:
 		for i in range(scenes.size()):
-			if scenes[i].get_name() == str(current_scene_path):
-				current_index = i
-				break
+			print(scenes[i])
+			#if scenes[i].get_name() == current_scene_path:
+				#current_index = i
+				#break
+		#print(current_index)
 
 func _on_transition_1_body_entered(body):
 	if body.is_in_group("player"):
